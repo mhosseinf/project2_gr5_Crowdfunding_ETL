6ef0b223a9dff0c28099c51137fbb47aaea55a9c
lakigit <Jay.Somiuru@scservices.com.au>
2023-10-14 22:35:20 +0800

    added NOT NULL condition for column on SQL schema

--- Crowdfunding_ETL/crowdfunding_db_schema.sql
+++ Crowdfunding_ETL/crowdfunding_db_schema.sql
@@ -6,25 +6,25 @@ DROP TABLE category;
 --create category table 
 
 CREATE TABLE category(
-	category_id VARCHAR(30),
-	category VARCHAR(50),
+	category_id VARCHAR(30) NOT NULL,
+	category VARCHAR(50) NOT NULL,
 	PRIMARY KEY(category_id)
 );
 
 --create subcategory table
 
 CREATE TABLE subcategory(
-	subcategory_id VARCHAR(30),
-	subcategory VARCHAR(50),
+	subcategory_id VARCHAR(30) NOT NULL,
+	subcategory VARCHAR(50) NOT NULL,
 	PRIMARY KEY(subcategory_id)
 );
 
 --create contacts table
 
 CREATE TABLE contacts(
-	contact_id INT,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
+	contact_id INT NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
 	email VARCHAR,
 	PRIMARY KEY(contact_id)
 );
@@ -32,22 +32,22 @@ CREATE TABLE contacts(
 --create campaign table
 
 CREATE TABLE campaign(
-	cf_id INT,
-	contact_id INT,
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
 	FOREIGN KEY(contact_id) REFERENCES contacts(contact_id),
-	company_name VARCHAR,
-	description TEXT,
-	goal FLOAT,
-	pledge FLOAT,
-	outcome VARCHAR,
-	backers_count INT,
-	country VARCHAR,
-	currency VARCHAR,
-	launched_date DATE,
-	end_date DATE,
-	category_id VARCHAR,
+	company_name VARCHAR NOT NULL,
+	description TEXT NOT NULL,
+	goal FLOAT NOT NULL,
+	pledge FLOAT NOT NULL,
+	outcome VARCHAR NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR NOT NULL,
+	currency VARCHAR NOT NULL,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR NOT NULL,
 	FOREIGN KEY(category_id) REFERENCES category(category_id),
-	subcategory_id VARCHAR,
+	subcategory_id VARCHAR NOT NULL,
 	FOREIGN KEY(subcategory_id) REFERENCES subcategory(subcategory_id),
 	PRIMARY KEY(cf_id)
 );
